d575dc8f02bf4adb4254c81c8314288be405d5a3
Semakia <semakia.djaba@epitech.eu>
2026-08-10 23:11:29 +0200

    feat(terraform): add providers and variables tf files

---
+++ terraform/variables.tf
@@ -0,0 +1,17 @@
+
+variable "project_id" {
+    description = "ID of project GCP housing twitter-market-research"
+    type        = string
+}
+
+variable "region" {
+    description = "GCP default region for ressources
+    type        = string
+    default     = "europe-west1"    
+}
+
+variable "zone" {
+    description = "GCP default zone for ressources"
+    type        = string
+    default     = "europe-west1-b"
+}
