083121e211033e7be3498892b33b4b98cf343266
Semakia <semakia.djaba@epitech.eu>
2026-08-20 01:59:14 +0200

    feat(terraform): add configs for VM's Instance

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -15,3 +15,10 @@ variable "zone" {
     type        = string
     default     = "europe-west1-b"
 }
+
+variable "machine_type" {
+  description = "Type de machine de la VM DWH (stack Kafka 3 brokers + MinIO + monitoring)"
+  type        = string
+  default     = "e2-standard-2"
+}
+
